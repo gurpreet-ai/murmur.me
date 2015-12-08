48340c7d749787a110f329d2d76079ee5d4442ef
gurusingh <gurusingh@bitbucket.org>
2015-12-07 22:34:52 -0500

    adding the main controller

--- murmer.me/public/app/controllers/mainController.js
+++ murmer.me/public/app/controllers/mainController.js
@@ -1,3 +1,18 @@
-// angular.module('mainController', [])
+angular.module('mainController', [])
 
-// .controller('main', function ($rootScope, ))
+.controller('main', function ($rootScope, $location, Auth) {
+	var vm = this;
+
+	vm.loggedIn = Auth.isLogged();
+
+	$rootScope.$on('$routeChangeStart', function() {
+		vm.loggedIn = Auth.isLoggedIn();
+
+		Auth.getUser().then(function (data) {
+			vm.user = data.data;
+		});
+	});
+
+	vm.data
+
+})
